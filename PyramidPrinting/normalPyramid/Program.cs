6c92ef10ccc5d7aeaaa63ba7f5058f1207dadb23
Alexey Brante <96073038+Fextice@users.noreply.github.com>
2022-07-27 17:44:19 +0300

    Add files via upload

---
+++ PyramidPrinting/normalPyramid/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int num = int.Parse(Console.ReadLine());
+            string star = "*";
+            // string space = " ";
+            for (int i = 0; i < num; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j <= i;j++)
+                {
+                    Console.Write(star);
+                }
+            }
+        }
+    }
+}
---
+++ PyramidPrinting/rotatedPyramid/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace rotatedPyramid
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string star = "*";
+            string space = " ";
+            int num = int.Parse(Console.ReadLine());
+            for (int i = 0; i <= num; i++)
+            {
+                Console.WriteLine();
+                for (int j = num; j > i; j--)
+                {
+                    Console.Write(space);
+                }
+
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write(star);
+                }
+            }
+        }
+    }
+}
